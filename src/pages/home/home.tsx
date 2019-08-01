7eb30b6d00ca11c4eb343123d3a6ce5a8da65da5
binaryify <binaryify@gmail.com>
2019-08-01 17:52:53 +0800

    修复样式错误

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,16 @@
-import Taro, { Component, Config } from "@tarojs/taro";
-import { View, Text, ScrollView } from "@tarojs/components";
-import { AtButton } from "taro-ui";
+import Taro, { Component, Config } from '@tarojs/taro';
+import { View, Text, ScrollView } from '@tarojs/components';
+import { AtButton } from 'taro-ui';
 // import { connect } from '@tarojs/redux'
 // import Api from '../../utils/request'
 // import Tips from '../../utils/tips'
-import { MAINHOST } from "../../config";
-import { homeProps, homeState } from "./home.interface";
-import "./home.scss";
-import ComponentBaseNavigation from "../../components/ComponentHomeNavigation/componentHomeNavigation";
-import ImageView from "../../components/ImageView/ImageView";
-import produce from "immer";
-import classnames from "classnames";
+import { MAINHOST } from '../../config';
+import { homeProps, homeState } from './home.interface';
+import './home.scss';
+import ComponentBaseNavigation from '../../components/ComponentHomeNavigation/componentHomeNavigation';
+import ImageView from '../../components/ImageView/ImageView';
+import produce from 'immer';
+import classnames from 'classnames';
 // import { } from '../../components'
 
 // @connect(({ home }) => ({
@@ -19,13 +19,13 @@ import classnames from "classnames";
 
 class home extends Component<homeProps, homeState> {
     config: Config = {
-        navigationBarTitleText: ""
+        navigationBarTitleText: ''
     };
     constructor(props: homeProps) {
         super(props);
         this.state = {
             pushList: [],
-            chooseType: "push",
+            chooseType: 'push',
             noticeList: []
         };
     }
@@ -83,28 +83,28 @@ class home extends Component<homeProps, homeState> {
         // ];
         const noticeList = [
             {
-                type: "社区",
-                title: "2019秋季放假安排",
-                date: "7月20日",
-                desc: "放假安排描述",
-                projectStatusText: "招募中",
+                type: '社区',
+                title: '2019秋季放假安排',
+                date: '7月20日',
+                desc: '放假安排描述',
+                projectStatusText: '招募中',
                 id: 1
             },
             {
-                type: "活动",
-                title: "篮球",
-                date: "7月20日",
-                desc: "中银花园",
-                projectStatusText: "招募中",
+                type: '活动',
+                title: '篮球',
+                date: '7月20日',
+                desc: '中银花园',
+                projectStatusText: '招募中',
                 id: 2
             }
         ];
         pushList.forEach(item => {
-            item["status"] = false;
-            item["type"] = "活动";
+            item['status'] = false;
+            item['type'] = '活动';
         });
         noticeList.forEach(item => {
-            item["status"] = false;
+            item['status'] = false;
         });
 
         this.setState({
@@ -123,9 +123,9 @@ class home extends Component<homeProps, homeState> {
     goDetail(type, item) {
         Taro.navigateTo({
             url:
-                "/pages/eventCardDetail/eventCardDetail?type=" +
+                '/pages/eventCardDetail/eventCardDetail?type=' +
                 type +
-                "&id=" +
+                '&id=' +
                 item.id
         });
     }
@@ -141,7 +141,7 @@ class home extends Component<homeProps, homeState> {
         // console.log({ event ,item});
         event.stopPropagation();
         const type =
-            this.state.chooseType === "push" ? "pushList" : "noticeList";
+            this.state.chooseType === 'push' ? 'pushList' : 'noticeList';
         const list = produce(this.state[type], draftState => {
             draftState.forEach(active => {
                 if (active.id === item.id) {
@@ -150,7 +150,7 @@ class home extends Component<homeProps, homeState> {
             });
         });
 
-        if (type === "pushList") {
+        if (type === 'pushList') {
             this.setState({
                 pushList: list
             });
@@ -163,126 +163,138 @@ class home extends Component<homeProps, homeState> {
     }
     getBg(type) {
         return {
-            活动: "green",
-            社区: "red",
-            项目: "blue",
-            预约: "orange",
-            计划: "yellow"
+            活动: 'green',
+            社区: 'red',
+            项目: 'blue',
+            预约: 'orange',
+            计划: 'yellow'
         }[type];
     }
 
     render() {
         const data =
-            this.state.chooseType === "push"
+            this.state.chooseType === 'push'
                 ? this.state.pushList
                 : this.state.noticeList;
-        const listComponent = data.map(item => {
-            return (
-                <View
-                    key={item.id}
-                    className={classnames("li-ele card", {
-                        active: item.status
-                    })}
-                    onClick={() => {
-                        this.goDetail(this.state.chooseType, item);
-                    }}
-                >
-                    <View className="main-panel">
+        const listComponent =
+            data.length > 0 ? (
+                data.map(item => {
+                    return (
                         <View
-                            className={classnames("tag", this.getBg(item.type))}
+                            key={item.id}
+                            className={classnames('li-ele card', {
+                                active: item.status
+                            })}
+                            onClick={() => {
+                                this.goDetail(this.state.chooseType, item);
+                            }}
                         >
-                            {item.type}
-                        </View>
-                        <View className="left-panel">
-                            <View className="avatar" />
-                            {/* <ImageView img-class="avatar" pathId=""></ImageView> */}
-                            <View
-                                className="status-button"
-                                style={
-                                    item.type === "项目" ? "" : "display:none"
-                                }
-                            >
-                                招募中
-                            </View>
-                        </View>
-                        <View className="infoPanel">
-                            <View className="title">
-                                {item.eventInfo.title}
-                            </View>
-                            <View className="date">
-                                {item.eventInfo.startDate}
-                            </View>
-                            <View className="desc">
-                                {item.eventInfo.description}
-                            </View>
-                        </View>
-                        <View onClick={this.toggle.bind(this, item)}>
-                            <AtButton className="sub-button">报名</AtButton>
-                        </View>
+                            <View className='main-panel'>
+                                <View
+                                    className={classnames(
+                                        'tag',
+                                        this.getBg(item.type)
+                                    )}
+                                >
+                                    {item.type}
+                                </View>
+                                <View className='left-panel'>
+                                    <View className='avatar' />
+                                    {/* <ImageView img-class="avatar" pathId=""></ImageView> */}
+                                    <View
+                                        className='status-button'
+                                        style={
+                                            item.type === '项目'
+                                                ? ''
+                                                : 'display:none'
+                                        }
+                                    >
+                                        招募中
+                                    </View>
+                                </View>
+                                <View className='infoPanel'>
+                                    <View className='title'>
+                                        {item.eventInfo.title}
+                                    </View>
+                                    <View className='date'>
+                                        {item.eventInfo.startDate}
+                                    </View>
+                                    <View className='desc'>
+                                        {item.eventInfo.description}
+                                    </View>
+                                </View>
+                                <View onClick={this.toggle.bind(this, item)}>
+                                    <AtButton className='sub-button'>
+                                        报名
+                                    </AtButton>
+                                </View>
 
-                        <View className="at-icon at-icon-chevron-right icon-right" />
-                    </View>
-                    {item.status ? (
-                        <View className="action-panel">
-                            <View
-                                className="action-item"
-                                onClick={this.cancel.bind(this, item)}
-                            >
-                                <View className="at-icon at-icon-close icon-close" />
-                                <Text className="text">取消</Text>
+                                <View className='at-icon at-icon-chevron-right icon-right' />
                             </View>
-                            <View
-                                className="action-item"
-                                onClick={this.join.bind(this, item)}
-                            >
-                                <View className="at-icon at-icon-help icon-help" />
-                                <Text className="text">可能参加</Text>
-                            </View>
+                            {item.status ? (
+                                <View className='action-panel'>
+                                    <View
+                                        className='action-item'
+                                        onClick={this.cancel.bind(this, item)}
+                                    >
+                                        <View className='at-icon at-icon-close icon-close' />
+                                        <Text className='text'>取消</Text>
+                                    </View>
+                                    <View
+                                        className='action-item'
+                                        onClick={this.join.bind(this, item)}
+                                    >
+                                        <View className='at-icon at-icon-help icon-help' />
+                                        <Text className='text'>可能参加</Text>
+                                    </View>
+                                </View>
+                            ) : null}
                         </View>
-                    ) : null}
-                </View>
+                    );
+                })
+            ) : (
+                <View>暂无数据</View>
             );
-        });
         const tabList = [
             {
-                type: "push",
-                name: "推送",
-                iconClass: "at-icon-bookmark"
+                type: 'push',
+                name: '推送',
+                iconClass: 'at-icon-bookmark'
             },
             {
-                type: "notice",
-                name: "提醒",
-                iconClass: "at-icon-bell"
+                type: 'notice',
+                name: '提醒',
+                iconClass: 'at-icon-bell'
             }
         ];
         return (
-            <View className="home-wrap">
-                <ComponentBaseNavigation type="normal" />
+            <View className='home-wrap'>
+                <ComponentBaseNavigation type='normal' />
                 <ScrollView
-                    className="scrollview"
+                    className='scrollview'
                     scrollY
                     style={
-                        this.state.chooseType === "push" ? "" : "display:none"
+                        this.state.chooseType === 'push' ? '' : 'display:none'
                     }
                 >
-                    <View className="ul-ele">{listComponent}</View>
+                    <View className='ul-ele'>{listComponent}</View>
                 </ScrollView>
 
                 <ScrollView
-                    className="scrollview"
+                    className='scrollview'
                     scrollY
                     style={
-                        this.state.chooseType === "notice" ? "" : "display:none"
+                        this.state.chooseType === 'notice' ? '' : 'display:none'
                     }
                 >
-                    <View className="ul-ele">{listComponent}</View>
+                    <View className='ul-ele'>{listComponent}</View>
                 </ScrollView>
 
-                <View className="bottom-tab-panel">
+                <View className='bottom-tab-panel'>
                     {tabList.map(item => {
                         return (
                             <View
-                                className={classnames("tab-item", {
+                                className={classnames('tab-item', {
                                     active: this.state.chooseType === item.type
                                 })}
                                 onClick={() => {
@@ -292,7 +304,7 @@ class home extends Component<homeProps, homeState> {
                             >
                                 <View
                                     className={classnames(
-                                        "at-icon",
+                                        'at-icon',
                                         item.iconClass
                                     )}
                                 />
