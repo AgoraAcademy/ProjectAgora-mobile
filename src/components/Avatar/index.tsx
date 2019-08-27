583537da89863028705ef40d4bf41c9642255674
binaryify <binaryify@gmail.com>
2019-08-27 18:37:20 +0800

    improve

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,38 +1,45 @@
-import Taro, { Component } from '@tarojs/taro';
-import { View } from '@tarojs/components';
-import { StateInterface, PropsInterface } from './interface';
-import './style.scss';
-import { colorList } from '../../globalData';
+import Taro, { Component } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { StateInterface, PropsInterface } from './interface'
+import './style.scss'
+import { colorList } from '../../globalData'
 
 class ComponentHomeNavigation extends Component<
     PropsInterface,
     StateInterface
 > {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             bg: ''
-        };
+        }
     }
     componentDidMount() {
         this.setState({
             bg: this.getRandomColor()
-        });
+        })
     }
     getRandomColor() {
-        return colorList[Math.floor(Math.random() * colorList.length)];
+        return colorList[Math.floor(Math.random() * colorList.length)]
     }
     getFirstText(text) {
-        return text ? text.slice(0, 1) : '';
+        return text ? text.slice(0, 1) : ''
     }
 
     render() {
         return (
-            <View className='avatar' style={{ background: this.state.bg, height: this.props.size?this.props.size:'', width: this.props.size?this.props.size:'' }}>
+            <View
+              className='avatar'
+              style={{
+                    background: this.state.bg,
+                    height: this.props.size ? this.props.size : '',
+                    width: this.props.size ? this.props.size : ''
+                }}
+            >
                 {this.getFirstText(this.props.text)}
             </View>
-        );
+        )
     }
 }
 
-export default ComponentHomeNavigation;
+export default ComponentHomeNavigation
